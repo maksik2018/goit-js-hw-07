838a0134c3775f840e1390a46f3d58345800fb16
Maksim <gashenko100@ukr.net>
2021-05-30 23:01:12 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -12,4 +12,32 @@
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+const inputEl = document.querySelector('[type="number"]');
+const renderEl = document.querySelector('[data-action="render"]');
+const destroyEl = document.querySelector('[data-action="destroy"]');
+const boxesEl = document.querySelector('#boxes');
+
+renderEl.addEventListener('click', onCreateBoxes);
+destroyEl.addEventListener('click', OnDestroyBoxes);
+
+
+function onCreateBoxes() {
+    let basicSize = 30;
+    let num = inputEl.value;
+
+   for (let i = 0; i < num; i+=1) {
+       const size = basicSize += 10;
+    const div = document.createElement('div');
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgb( ${random()} , ${random()} , ${random()} )`;
+    boxesEl.appendChild(div);
+  }
+};
+
+function OnDestroyBoxes() {
+    boxesEl.innerHTML = '';
+};
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
 
